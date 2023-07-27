401a2681e04ccc4567e8a67e07d997974fdbe917
Pablo Rossetti <prensap.ross@gmail.com>
2023-07-27 13:48:37 -0300

    second challenge

--- ejercicios-algoritmos/sesion-4/encontrar-al-aislado/index.ts
+++ ejercicios-algoritmos/sesion-4/encontrar-al-aislado/index.ts
@@ -1,4 +1,6 @@
 export default function buscarAislado(numeros: number[]): number {
   // TODO: implementar
-  return 0;
+ const aisladoEsPar = numeros.filter((numero) => numero % 2 === 0).length ===1;
+
+ return numeros.find((numero) => aisladoEsPar ? numero % 2 === 0 : numero % 2 !==0) as number;
 }
