1e7eba795207d08e33397b7c69126b95f4710fe4
Pablo Rossetti <prensap.ross@gmail.com>
2023-07-31 13:21:39 -0300

    add comment

--- ejercicios-algoritmos/sesion-4/contar-los-lenguajes/index.ts
+++ ejercicios-algoritmos/sesion-4/contar-los-lenguajes/index.ts
@@ -1,7 +1,7 @@
 import type { Developer } from "./types";
 
 export default function contarLosLenguajes(developers: Developer[]): Record<string, number> {
-  // TODO: implementar
+  //Resolución
   return developers.reduce((lenguajes, {language}) => {
   if(!lenguajes[language]){
     lenguajes[language] = 0;
